8aae87b888d82c4f98be7036e637d337e806e20e
Shige <83544518+ST0927@users.noreply.github.com>
2023-09-05 16:51:19 +0900

    Add files via upload

---
+++ def fibonacci(n):.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n <= 0:
+        raise ValueError("nは正の整数を入力してください")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+try:
+    n = int(input("nを入力してください: "))
+    result = fibonacci(n)
+    print(f"{n}番目のフィボナッチ数は {result} です")
+except ValueError as ve:
+    print(f"nは正の整数を入力してください: {ve}")
+except RecursionError:
+    print("計算が深すぎます。nの値を小さくしてください。")
+except Exception as e:
+    print(f"予期せぬエラーが発生しました: {e}")
